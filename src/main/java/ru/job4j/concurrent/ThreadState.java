c42f3ae76d5f2b75c4bb66e1a4f76db9c8b6e40d
nesterenko-marina99 <miss.nesterenko.marina@gmail.com>
2021-09-01 18:28:03 +0400

    1.1 Состояние нити

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        System.out.println("first " + first.getState());
+        first.start();
+        System.out.println("second " + second.getState());
+        second.start();
+        while ((first.getState() != Thread.State.TERMINATED) && (second.getState() != Thread.State.TERMINATED)) {
+            System.out.println("first " + first.getState());
+            System.out.println("second " + second.getState());
+        }
+        System.out.println("first " + first.getState());
+        System.out.println("second " + second.getState());
+    }
+}
